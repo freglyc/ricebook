4ea2d625bdd2eebcb1551417bc5c0a5ebda7a924
freglyc <cdf7@rice.edu>
2019-12-06 00:27:10 -0600

    Updates

---
+++ precache-manifest.9eb683d786580569c82867f8b770f06a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef92978e9e33b9401bb5da6dd6d3589",
+    "url": "/ricebook/index.html"
+  },
+  {
+    "revision": "af1cae4ad92f1bb4412f",
+    "url": "/ricebook/static/css/main.c1f03c6e.chunk.css"
+  },
+  {
+    "revision": "8649a78c004352e88f0e",
+    "url": "/ricebook/static/js/2.6e0103d8.chunk.js"
+  },
+  {
+    "revision": "af1cae4ad92f1bb4412f",
+    "url": "/ricebook/static/js/main.3920d00d.chunk.js"
+  },
+  {
+    "revision": "f748d0fd83e5039d037f",
+    "url": "/ricebook/static/js/runtime-main.ad06728f.js"
+  },
+  {
+    "revision": "a1ebff360fd1e8cb4f49e034a6512d50",
+    "url": "/ricebook/static/media/add-image.a1ebff36.svg"
+  },
+  {
+    "revision": "e5f2f68b4101f9e804b765b9c90671bc",
+    "url": "/ricebook/static/media/landing.e5f2f68b.svg"
+  },
+  {
+    "revision": "96244dfc01e7071bd1bb87ed78ff4429",
+    "url": "/ricebook/static/media/new-post.96244dfc.svg"
+  },
+  {
+    "revision": "64193f8c4b602b83033786d62301c9b4",
+    "url": "/ricebook/static/media/placeholder.64193f8c.jpg"
+  },
+  {
+    "revision": "40d44dcce4f824da2e730268de28ca98",
+    "url": "/ricebook/static/media/search.40d44dcc.svg"
+  }
+]);
